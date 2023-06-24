31af10ad0f4ed14ad21f70bd955483a2e31c92d2
Abdelatif H <ab.hassaine@protonmail.com>
2023-06-24 21:25:07 +0100

    Add header code from HTML mockup

---
+++ wp-content/themes/planty/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <?php wp_head(); ?>
+</head>
+<body>
+<header class="header">
+    <a href="index.html"><img src="./assets/img/logo.png" alt="logo Planty" class="header__logo"></a>
+
+    <nav>
+        <ul class="header__nav">
+            <li><a href="contact.html">Nous rencontrer</a></li>
+            <li><a href="#">Admin</a></li>
+        </ul>
+
+        <a href="order.html" class="header__cta btn-primary">Commander</a>
+    </nav>
+</header>
